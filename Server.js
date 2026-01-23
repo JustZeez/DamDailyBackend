f70de5ae5b167b917d3035ed7e182029564df8bb
JustZeez <justzeez65@gmail.com>
2026-01-23 15:26:29 +0100

    Fix Cors Error

--- Server.js
+++ Server.js
@@ -28,8 +28,8 @@ app.use(helmet());
 app.use(limiter);
 app.use(cors({
     origin: process.env.FRONTEND_URL ||
-     'http://localhost:5173',
-    // 'https://miwacakesandtreatsfrontend.vercel.app',
+    //  'http://localhost:5173',
+    'https://damdaily.vercel.app',
     credentials:true
 }));
 app.use(express.json());
